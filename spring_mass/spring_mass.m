dd484fcaf01132d1627a15b7aad0c7b74f056206
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-01 16:21:20 -0700

    Changed algorithm to sqp, much better results

--- spring_mass/spring_mass.m
+++ spring_mass/spring_mass.m
@@ -1,12 +1,12 @@
 global gridN mass spring damp
-gridN = 30;
+gridN = 40;
 mass = 10;
 spring = 50;
 damp = 1;
 
 tic
 % Minimize the simulation time
-time_min = @(x) (x(1))^2;
+time_min = @(x) x(1)^2;
 % The initial parameter guess; 1 second, fifty lengths, fifty lengthdirs,
 % fifty actuated lengths
 if exist('optimal','var')
@@ -22,12 +22,12 @@ Aeq = [];
 Beq = [];
 % Lower bound the simulation time at zero seconds, and bound the
 % accelerations between -10 and 30
-lb = [0;   ones(gridN, 1) * 0; ones(gridN, 1) * -Inf; ones(gridN, 1) * -5];
-ub = [Inf; ones(gridN, 1) * 1;  ones(gridN, 1) * Inf;  ones(gridN, 1) * 5];
+lb = [0;   ones(gridN, 1) * 0; ones(gridN, 1) * -Inf; ones(gridN, 1) * -10];
+ub = [Inf; ones(gridN, 1) * 1;  ones(gridN, 1) * Inf;  ones(gridN, 1) * 10];
 % Options for fmincon
 options = optimoptions(@fmincon, 'TolFun', 0.00000001, 'MaxIter', 10000, ...
                        'MaxFunEvals', 100000, 'Display', 'iter', ...
-                       'DiffMinChange', 0.001);
+                       'DiffMinChange', 0.001, 'Algorithm', 'sqp');
 % Solve for the best simulation time + control input
 optimal = fmincon(time_min, x0, A, b, Aeq, Beq, lb, ub, ...
               @spring_mass_constraints, options);
