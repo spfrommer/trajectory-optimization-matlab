bd1d4ad7302c6d928687e687f563a101baf5cf33
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-01 16:10:31 -0700

    Added quadrature and squared simulation time

--- spring_mass/spring_mass.m
+++ spring_mass/spring_mass.m
@@ -1,19 +1,19 @@
 global gridN mass spring damp
-gridN = 20;
+gridN = 30;
 mass = 10;
 spring = 50;
 damp = 1;
 
 tic
 % Minimize the simulation time
-time_min = @(x) x(1);
+time_min = @(x) (x(1))^2;
 % The initial parameter guess; 1 second, fifty lengths, fifty lengthdirs,
 % fifty actuated lengths
 if exist('optimal','var')
     disp('Using previous solution as starting guess...');
     x0 = optimal;
 else
-    x0 = [1; linspace(0,1,gridN)'; linspace(0,1,gridN)'; ones(gridN, 1) * 5];
+    x0 = [1; ones(gridN, 1) * 0.9; ones(gridN, 1); ones(gridN, 1)];
 end
 % No linear inequality or equality constraints
 A = [];
@@ -22,11 +22,12 @@ Aeq = [];
 Beq = [];
 % Lower bound the simulation time at zero seconds, and bound the
 % accelerations between -10 and 30
-lb = [0;   ones(gridN * 1, 1) * 0; ones(gridN * 1, 1) * -Inf; ones(gridN, 1) * -200];
-ub = [Inf; ones(gridN * 1, 1) * 1;  ones(gridN * 1, 1) * Inf;  ones(gridN, 1) * 200];
+lb = [0;   ones(gridN, 1) * 0; ones(gridN, 1) * -Inf; ones(gridN, 1) * -5];
+ub = [Inf; ones(gridN, 1) * 1;  ones(gridN, 1) * Inf;  ones(gridN, 1) * 5];
 % Options for fmincon
-options = optimset('TolFun', 0.00000001, 'MaxIter', 100000, ...
-                   'MaxFunEvals', 100000);
+options = optimoptions(@fmincon, 'TolFun', 0.00000001, 'MaxIter', 10000, ...
+                       'MaxFunEvals', 100000, 'Display', 'iter', ...
+                       'DiffMinChange', 0.001);
 % Solve for the best simulation time + control input
 optimal = fmincon(time_min, x0, A, b, Aeq, Beq, lb, ub, ...
               @spring_mass_constraints, options);
--- spring_mass/spring_mass_constraints.m
+++ spring_mass/spring_mass_constraints.m
@@ -10,8 +10,7 @@ function [ c, ceq ] = spring_mass_constraints( x )
     lengthdirs      = x(2 + gridN     : 1 + gridN * 2);
     actlengths      = x(2 + gridN * 2 : end);
     
-    % Constrain initial position to be one and velocity to be zero
-    ceq = [lengths(1) - 1; lengthdirs(1)];
+    ceq = ones(2 * (gridN - 1), 1);
     for i = 1 : gridN-1
         % The state at the beginning of the time interval
         x_i = [lengths(i); lengthdirs(i)];
@@ -19,22 +18,22 @@ function [ c, ceq ] = spring_mass_constraints( x )
         x_n = [lengths(i+1); lengthdirs(i+1)];
         
         A = [0 1; -spring/mass -damp/mass];
-        B = [0; actlengths(i)*spring/mass];
+        Bi = [0; actlengths(i)*spring/mass];
+        Bn = [0; actlengths(i+1)*spring/mass];
         
-        xdot_i = A * x_i + B;
-        ceq = [ceq ; x_n - (x_i + delta_time * xdot_i)];
         % The time derivative of the state at the beginning of the time
         % interval
-        % xdot_i = [lengthdirs(i); -spring(i)];
+        xdot_i = A * x_i + Bi;
         % The time derivative of the state at the end of the time interval
-        %xdot_n = [vels(i+1); accs(i+1)];
-        
+        xdot_n = A * x_n + Bn;
         % The end position of the time interval calculated using quadrature
-        %xend = x_i + delta_time * (xdot_i + xdot_n) / 2;
-        % Constrain the end position of the current time interval to be
-        % equal to the starting position of the next time interval
-        %ceq = [ceq ; x_n - xend];
+        xend = x_i + delta_time * (xdot_i + xdot_n) / 2;
+        % Constrain the end state of the current time interval to be
+        % equal to the starting state of the next time interval
+        ceq(i*2-1:i*2) = x_n - xend;
     end
+    % Constrain initial position to be one and velocity to be zero
+    ceq = [ceq; lengths(1) - 1; lengthdirs(1)];
     % Constrain end position to 1 and end velocity to 0
-    ceq = [ceq ; lengths(end) - 0.8; lengthdirs(end)];
+    ceq = [ceq; lengths(end) - 0.8; lengthdirs(end)];
 end
