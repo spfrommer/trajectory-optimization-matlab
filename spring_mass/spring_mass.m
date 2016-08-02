cb5bcba2f84c48aec02ca8109cfda70203a9a73b
Samuel Pfrommer <sam.pfrommer@gmail.com>
2016-08-02 14:51:45 -0700

    Changed spring mass example control variable

--- spring_mass/spring_mass.m
+++ spring_mass/spring_mass.m
@@ -8,12 +8,12 @@ tic
 % Minimize the simulation time
 time_min = @(x) x(1)^2;
 % The initial parameter guess; 1 second, fifty lengths, fifty lengthdirs,
-% fifty actuated lengths
+% fifty actuated lengths, fifty actlengthdirs, fifty actlengthddirs
 if exist('optimal','var')
     disp('Using previous solution as starting guess...');
     x0 = optimal;
 else
-    x0 = [1; ones(gridN, 1) * 0.9; ones(gridN, 1); ones(gridN, 1)];
+    x0 = [1; ones(gridN, 1) * 0.9; ones(gridN, 1); ones(gridN * 3, 1)];
 end
 % No linear inequality or equality constraints
 A = [];
@@ -22,8 +22,8 @@ Aeq = [];
 Beq = [];
 % Lower bound the simulation time at zero seconds, and bound the
 % accelerations between -10 and 30
-lb = [0;   ones(gridN, 1) * 0; ones(gridN, 1) * -Inf; ones(gridN, 1) * -10];
-ub = [Inf; ones(gridN, 1) * 1;  ones(gridN, 1) * Inf;  ones(gridN, 1) * 10];
+lb = [0;   ones(gridN, 1) * 0; ones(gridN * 3, 1) * -Inf; ones(gridN, 1) * -10];
+ub = [Inf; ones(gridN, 1) * 1; ones(gridN * 3, 1) * Inf;  ones(gridN, 1) * 10];
 % Options for fmincon
 options = optimoptions(@fmincon, 'TolFun', 0.00000001, 'MaxIter', 10000, ...
                        'MaxFunEvals', 100000, 'Display', 'iter', ...
@@ -39,16 +39,28 @@ times = 0 : delta_time : sim_time - delta_time;
 % Get the state + accelerations (control inputs) out of the vector
 lengths         = optimal(2             : 1 + gridN);
 lengthdirs      = optimal(2 + gridN     : 1 + gridN * 2);
-actlengths      = optimal(2 + gridN * 2 : end);
+actlengths      = optimal(2 + gridN * 2 : 1 + gridN * 3);
+actlengthdirs   = optimal(2 + gridN * 3 : 1 + gridN * 4);
+actlengthddirs  = optimal(2 + gridN * 4 : end);
 
 [c, ceq] = spring_mass_constraints(optimal);
 
 % Make the plots
 figure();
 plot(times, actlengths);
-title('Control Input (Actuated Length) vs Time');
+title('Actuated Length vs Time');
 xlabel('Time (s)');
 ylabel('Actuated Length (m)');
+figure();
+plot(times, actlengthdirs);
+title('Actuated Length Derivatives vs Time');
+xlabel('Time (s)');
+ylabel('Actuated Length Derivative');
+figure();
+plot(times, actlengthddirs);
+title('Actuated Length Second Derivatives vs Time');
+xlabel('Time (s)');
+ylabel('Actuated Length Second Derivatives');
 figure();
 plot(times, lengthdirs);
 title('Length Derivatives vs Time');
--- spring_mass/spring_mass_constraints.m
+++ spring_mass/spring_mass_constraints.m
@@ -8,18 +8,24 @@ function [ c, ceq ] = spring_mass_constraints( x )
     % Get the states / inputs out of the vector
     lengths         = x(2             : 1 + gridN);
     lengthdirs      = x(2 + gridN     : 1 + gridN * 2);
-    actlengths      = x(2 + gridN * 2 : end);
+    actlengths      = x(2 + gridN * 2 : 1 + gridN * 3);
+    actlengthdirs   = x(2 + gridN * 3 : 1 + gridN * 4);
+    actlengthddirs  = x(2 + gridN * 4 : end);
     
-    ceq = ones(2 * (gridN - 1), 1);
+    ceq = ones(4 * (gridN - 1), 1);
     for i = 1 : gridN-1
         % The state at the beginning of the time interval
-        x_i = [lengths(i); lengthdirs(i)];
+        x_i = [lengths(i); lengthdirs(i); actlengths(i); actlengthdirs(i)];
         % What the state should be at the end of the time interval
-        x_n = [lengths(i+1); lengthdirs(i+1)];
+        x_n = [lengths(i+1); lengthdirs(i+1); ...
+               actlengths(i+1); actlengthdirs(i+1)];
         
-        A = [0 1; -spring/mass -damp/mass];
-        Bi = [0; actlengths(i)*spring/mass];
-        Bn = [0; actlengths(i+1)*spring/mass];
+        A = [0 1 0 0;
+            -spring/mass -damp/mass spring/mass damp/mass;
+             0 0 0 1;
+             0 0 0 0];
+        Bi = [0; 0; 0; actlengthddirs(i)];
+        Bn = [0; 0; 0; actlengthddirs(i+1)];
         
         % The time derivative of the state at the beginning of the time
         % interval
@@ -30,10 +36,10 @@ function [ c, ceq ] = spring_mass_constraints( x )
         xend = x_i + delta_time * (xdot_i + xdot_n) / 2;
         % Constrain the end state of the current time interval to be
         % equal to the starting state of the next time interval
-        ceq(i*2-1:i*2) = x_n - xend;
+        ceq((i-1)*4+1: i*4) = x_n - xend;
     end
     % Constrain initial position to be one and velocity to be zero
-    ceq = [ceq; lengths(1) - 1; lengthdirs(1)];
+    ceq = [ceq; lengths(1) - 1; lengthdirs(1); actlengths(1) - 1; actlengthdirs(i)];
     % Constrain end position to 1 and end velocity to 0
-    ceq = [ceq; lengths(end) - 0.8; lengthdirs(end)];
+    ceq = [ceq; lengths(end) - 0.8];
 end
